8a028bf0a2f92271f3c25848c18113b0e25914cd
Shaheer <Shaheer@harambee.co.za>
2021-07-03 13:48:30 +0200

    api

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -23,9 +23,9 @@ $(function () {
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
-                // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
-                // ------------------------------------------------------  
+                // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  --------
+                url: "https://ullv137880.execute-api.eu-west-1.amazonaws.com/default/LambdaFunction_Predict",
+                // ------------------------------------------------------
                 type: "POST",
                 data: JSON.stringify(json),
                 cache: false,
